2dce6a6f98c7616f73d95bc34af41a6b876b38b2
Aaron Berns <anberns@gmail.com>
2019-01-06 15:00:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,3 @@
-Open this lab with `learn open` and run your tests with `learn`.
-
-### 1. Define `Dog` in `lib/dog.rb`
-
-Open `lib/dog.rb` and add a class definition for a `Dog` class.
-
-### 2. Make 3 dogs in `lib/dog.rb`
-
-Under your `Dog` class definition, create three dogs in local variables, `fido`, `snoopy`, and `lassie`.
-
 class Dog 
 end
 
